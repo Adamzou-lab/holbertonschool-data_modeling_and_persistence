50b9b11e80043339eb9010798f8d80bf46e0f1cc
Adamzou-lab <adamzpro@icloud.com>
2026-06-05 22:12:23 +0200

    fix: correct join condition for courses and instructors

--- sql_join/8-courses-and-instructors.sql
+++ sql_join/8-courses-and-instructors.sql
@@ -1,4 +1,4 @@
 SELECT courses.title AS course_title, instructors.name AS instructor_name
 FROM instructors
-INNER JOIN courses ON courses.instructor_id = id
+INNER JOIN courses ON courses.instructor_id = instructors.id
 ORDER BY course_title ASC;
